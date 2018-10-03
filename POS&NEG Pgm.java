57c66f8478d9056759786b384f657bf5dbfbba25
RevathySRaj <43032770+RevathySRaj@users.noreply.github.com>
2018-10-03 12:32:32 +0530

    Add files via upload

---
+++ POS&NEG Pgm.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+  int num;
+  num=3;
+  if(num>0)
+    System.out.println(num +"is positive number");
+  else if(num<0)
+    System.out.println(num +"is negative number");
+  else
+    System.out.println("it is zero");
+     }
+}
